df95d59029eef08c5b201db434beb7e75f730011
Rafael Anacleto <rafael.anacleto12@gmail.com>
2020-09-19 22:28:34 -0300

    Configuring front-web for production

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://semana-devsuperior-1-rafael.herokuapp.com';
 
 const Records = () => {
 
